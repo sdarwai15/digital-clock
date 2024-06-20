d52ebab456036e1f5f121bb09b9e7604ab5fbce0
sdarwai15 <sdarwai15@gmail.com>
2024-06-20 06:48:14 +0530

    built digital clock

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [seconds, setSeconds] = useState(55);
+	const [minutes, setMinutes] = useState(59);
+	const [hours, setHours] = useState(23);
+
+	// updating seconds
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setSeconds((prevSeconds) => prevSeconds + 1);
+		}, 1000);
+		return () => clearInterval(interval);
+	}, []);
+
+	// updating minutes
+	useEffect(() => {
+		if (seconds === 60) {
+			setSeconds(0);
+			setMinutes((prevMinutes) => prevMinutes + 1);
+		}
+	}, [seconds]);
+
+	// updating hours
+	useEffect(() => {
+		if (minutes === 60) {
+			setMinutes(0);
+			setHours((prevHours) => prevHours + 1);
+		}
+	}, [minutes]);
+
+	// resetting the clock
+	useEffect(() => {
+		if (hours === 24) {
+			setHours(0);
+			setMinutes(0);
+			setSeconds(0);
+		}
+	}, [hours]);
+
+	return (
+		<div className="App">
+			<h1>DIGITAL CLOCK</h1>
+			<div className="clock">
+				{hours < 10 ? `0${hours}` : hours}:
+				{minutes < 10 ? `0${minutes}` : minutes}:
+				{seconds < 10 ? `0${seconds}` : seconds}
+			</div>
+		</div>
+	);
 }
 
 export default App;
